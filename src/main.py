8fdb6f03cd0f5a3f8f3adb26c3ec02e0f5b03952
Milo Craun <milocraun@gmail.com>
2022-12-24 11:55:42 -0500

    it generates something

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import graphviz
 
 
 def main():
@@ -8,13 +9,32 @@ def main():
     paths = generate_paths(sys.argv[1])
     for i in paths:
         print(i)
+    
+    digraph = graphviz.Digraph()
+    digraph.renderer = "cairo"
+    digraph.format = "png"
+    for i in paths:
+        digraph.node(i, i)
+
+    for file in paths:
+        f = open(file, "r")
+        lines = f.readlines()
+        for l in lines:
+            for i in paths:
+                if l.strip() in i:
+                    digraph.edge(file, i) 
+        f.close()
+    print(digraph.body) 
+    digraph.render()
+
 
 def generate_paths(path):
     walk = os.walk(path)
     file = list()
     for entry in walk:
         for filename in entry[2]:
-            file.append(entry[0] + "/" + filename)
+            if ".html" in filename:
+                file.append(entry[0] + "/" + filename)
     return file;
 
 if __name__ == "__main__":
