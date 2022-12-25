c1cffb7becbaa48a7bc3d0721b91d62956ad2085
Milo Craun <milocraun@gmail.com>
2022-12-25 08:36:37 -0500

    added code to use HTML parser

--- src/main.py
+++ src/main.py
@@ -1,18 +1,31 @@
 import os
 import sys
 import graphviz
+from html.parser import HTMLParser
+
+class HyperlinkParser(HTMLParser):
+    data = None
+    def handle_starttag(self, tag, attrs):
+        if tag == 'a':
+            self.data = attrs[0][1]
+        else:
+            self.data = None
+
 
 
 def main():
     if len(sys.argv) != 2:
+        print("Usage: python main.py directory")
+        print("Please do not include a trailing / after directory")
         exit(1)
     paths = generate_paths(sys.argv[1])
     for i in paths:
         print(i)
     
+    parser = HyperlinkParser()
     digraph = graphviz.Digraph()
     digraph.renderer = "cairo"
-    digraph.format = "png"
+    digraph.format = "svg"
     digraph.attr("graph", concentrate="true")
     digraph.strict = True
     for i in paths:
@@ -31,13 +44,31 @@ def main():
                     print(val, i)
                     if val == i:
                         edges.add((file, val))
+        f.close()
                         
-    print(edges)
-    for element in edges:
-        digraph.edge(element[0], element[1])
-    f.close()
-    digraph.render()
 
+
+    edges2 = set()
+    for file in paths:
+        f = open(file, "r")
+        for line in f:
+            parser.feed(line)
+            if parser.data != None:
+                for i in paths:
+                    if "milocraun.com/" + parser.data == i:
+                        edges2.add((file, "milocraun.com/" + parser.data))
+        f.close()
+    for i in edges2:
+        print(i)
+
+    print("\n")
+    for i in edges:
+        print(i)
+
+
+    for element in edges2:
+        digraph.edge(element[0], element[1])
+    digraph.render()
 
 def generate_paths(path):
     walk = os.walk(path)
